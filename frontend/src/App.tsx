7b5b734a0f7180c87e8ad6188ba381c59b08f9a6
Adnangad <gardobuyaadnan@gmail.com>
2025-04-28 07:21:24 +0300

    added a button component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from './Button'
+
+function App() {
+
+  return (
+    <>
+      <Button></Button>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Button.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+function Button() {
+    return(
+        <>
+        <button className="border-1 bg-blue-500 p-3 rounded-2xl text-white w-1/3">Submit</button>
+        </>
+    );
+}
+export default Button
